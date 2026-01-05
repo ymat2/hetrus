c979aa4477e51bbf334edfce4526d661406169dd
ymat2 <ymat2@users.noreply.github.com>
2026-01-05 18:29:54 +0900

    :sparkles: Introduce rust-htslib

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,18 @@
 use clap::Parser;
+use rust_htslib::bcf::{Reader, Read};
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Args {
-    #[arg(short, long)]
+    #[arg(short, long, help = "Provide your name.", default_value = "world")]
     name: String,
+
+    #[args(short, long, value_hint = clap::ValueHint::FilePath)]
+    input: PathBuf,
 }
 
 fn main() {
     let args = Args::parse();
     println!("Hello, {}!", args.name);
+    let mut bcf = Reader::from_path(args.input).expect("Error opening file.");
 }
