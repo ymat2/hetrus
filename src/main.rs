50dce9f7cb9603ce58f32eba92dc5a158ac44106
ymat2 <ymat2@users.noreply.github.com>
2026-01-05 17:17:52 +0900

    :sparkles: Introduce clap as cl parser

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[arg(short, long)]
+    name: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    println!("Hello, {}!", args.name);
 }
