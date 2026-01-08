2e80e13a8ca0cc613fc23251d424362de14d8da6
ymat2 <ymat2@users.noreply.github.com>
2026-01-08 20:42:20 +0900

    :sparkles: Calc He; Write output to TSV

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use clap::Parser;
 use rust_htslib::bcf::{Read, Reader, Record};
 use std::convert::TryFrom;
+use std::fs::File;
+use std::io::{BufWriter, Write};
 use std::path::PathBuf;
 
 #[derive(Parser, Debug)]
@@ -8,30 +10,38 @@ use std::path::PathBuf;
 struct Args {
     #[arg(short, long, value_hint = clap::ValueHint::FilePath, help = "VCF, bgzipped VCF, and BCF.")]
     input: PathBuf,
+    #[arg(short, long, value_hint = clap::ValueHint::FilePath, help = "Output file path.")]
+    out: PathBuf,
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Args::parse();
     let mut bcf = Reader::from_path(args.input)?;
+    let outfile = File::create(args.out)?;
+    let mut out = BufWriter::new(outfile);
+    let header = bcf.header().clone();
 
+    writeln!(out, "CHROM\tPOS\tHo\tHe")?;
     for record_result in bcf.records() {
         let record = record_result?;
-        let pos = record.pos();
-        // number of sample in the vcf
+        let rid = record.rid().expect("RID missing");
+        let chr = std::str::from_utf8(header.rid2name(rid)?)?;
+        let pos = record.pos() + 1;
         let sample_count = usize::try_from(record.sample_count()).unwrap();
 
         if !is_biallelic(&record) {
-            // println!("Locus {} is not biallelic site.", pos);
             continue;
         }
 
         let gt_count = count_genotypes(&record, sample_count)?;
         // haploid が含まれてしまっているが、"all missing" を除外することでひとまず対処？
         let observed_het = calc_observed_heterozygosity(gt_count, true)?;
-        println!(
-            "Locus: {}, GTs: {:?}, Ho: {:.4}",
-            pos, gt_count, observed_het
-        )
+        let expected_het = calc_expected_heterozygosity(gt_count, true)?;
+        writeln!(
+            out,
+            "{}\t{}\t{:.4}\t{:.4}",
+            chr, pos, observed_het, expected_het
+        )?;
     }
 
     Ok(())
@@ -44,11 +54,12 @@ fn is_biallelic(record: &Record) -> bool {
 fn count_genotypes(
     record: &Record,
     n_samples: usize,
-) -> Result<(u32, u32, u32), Box<dyn std::error::Error>> {
+) -> Result<(u32, u32, u32, u32), Box<dyn std::error::Error>> {
     let gts = record.genotypes()?;
 
-    let mut n_homo = 0;
+    let mut n_ref_homo = 0;
     let mut n_hetero = 0;
+    let mut n_alt_homo = 0;
     let mut n_missing = 0;
 
     for sample_index in 0..n_samples {
@@ -64,32 +75,56 @@ fn count_genotypes(
             }
         }
 
-        // diploid 前提
         match (ref_count, alt_count, missing_count) {
-            (2, 0, 0) | (0, 2, 0) => n_homo += 1,
+            (2, 0, 0) => n_ref_homo += 1,
+            (0, 2, 0) => n_alt_homo += 1,
             (1, 1, 0) => n_hetero += 1,
-            _ => n_missing += 1, // missing or partial missing
+            _ => n_missing += 1, // missing, partial missing, or not diploid
         }
     }
 
-    Ok((n_homo, n_hetero, n_missing))
+    Ok((n_ref_homo, n_hetero, n_alt_homo, n_missing))
 }
 
 fn calc_observed_heterozygosity(
-    gt_count: (u32, u32, u32),
+    gt_count: (u32, u32, u32, u32),
     ignore_missing: bool,
 ) -> Result<f32, Box<dyn std::error::Error>> {
     let ho: f32;
-    let (n_homo, n_hetero, n_missing) = gt_count;
+    let (n_ref_homo, n_hetero, n_alt_homo, n_missing) = gt_count;
     if ignore_missing {
-        if n_homo + n_hetero == 0 {
+        if n_ref_homo + n_hetero + n_alt_homo == 0 {
             ho = f32::NAN
         } else {
-            ho = (n_hetero) as f32 / (n_homo + n_hetero) as f32;
+            ho = (n_hetero) as f32 / (n_ref_homo + n_hetero + n_alt_homo) as f32;
         }
     } else {
-        ho = (n_hetero) as f32 / (n_homo + n_hetero + n_missing) as f32;
+        ho = (n_hetero) as f32 / (n_ref_homo + n_hetero + n_alt_homo + n_missing) as f32;
     }
 
     Ok(ho)
+}
+
+fn calc_expected_heterozygosity(
+    gt_count: (u32, u32, u32, u32),
+    ignore_missing: bool,
+) -> Result<f32, Box<dyn std::error::Error>> {
+    let af: f32;
+    let he: f32;
+    let (n_ref_homo, n_hetero, n_alt_homo, n_missing) = gt_count;
+    if ignore_missing {
+        if n_ref_homo + n_hetero + n_alt_homo == 0 {
+            he = f32::NAN
+        } else {
+            af = (1 * n_hetero + 2 * n_alt_homo) as f32
+                / (2 * (n_ref_homo + n_hetero + n_alt_homo)) as f32;
+            he = 2.0 * af * (1.0 - af)
+        }
+    } else {
+        af = (1 * n_hetero + 2 * n_alt_homo) as f32
+            / (2 * (n_ref_homo + n_hetero + n_alt_homo + n_missing)) as f32;
+        he = 2.0 * af * (1.0 - af)
+    }
+
+    Ok(he)
 }
